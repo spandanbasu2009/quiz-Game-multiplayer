de30dbb05bd697fba2224e800a30cc0c23895325
whitehatjrdemo1 <70006536+whitehatjrdemo1@users.noreply.github.com>
2021-06-01 22:32:22 +0530

    Add files via upload

--- js/Game.js
+++ js/Game.js
@@ -1,9 +1,9 @@
 class Game {
   constructor() {
-    this.maxPlayers = 15;
+    this.maxPlayers = 10;
     this.minPlayers = 2;
-    this.totalQuestions = 10;
-    this.questionWait = 10;
+    this.totalQuestions = 3;
+    this.questionWait = 5;
     this.answerWait = 5;
     this.waitTime = 10;
     this.questionNumber = 0;
@@ -14,9 +14,9 @@ class Game {
     this.questionEle = createElement("h2");
     this.roundPlayers = 0;
     this.messageEle = createElement("h2");
-    this.maxRound = 4;
+    this.maxRound = 3;
     this.playerScores = [];
-    this.roundType = "normal";
+    this.roundType = "fast";
     this.submitButton = createButton("Submit");
   }
 
@@ -113,7 +113,7 @@ class Game {
 
       this.submitButton.position(width / 2, height / 2 + 200);
       this.submitButton.mousePressed(() => {
-        console.log(counter + this.answerOptions.value());
+        //console.log(counter + this.answerOptions.value());
         player.updateTime(counter, this.answerOptions.value());
       });
 
@@ -140,14 +140,43 @@ class Game {
         playerCount = playerCountRef.val();
         player.getCount();
       }
+      var cArray = ["red", "blue", "yellow", "green", "black"];
+      colorChoice = createRadio();
+
+      database.ref("/").update({
+        colorArray: cArray,
+      });
+      database.ref("/colorArray").on("value", (data) => {
+        colorArray = data.val();
+        if (colorArray != []) {
+          colorChoice.hide();
+          clear();
+          colorChoice = createRadio();
+          for (var c in colorArray) {
+            colorChoice.option(colorArray[c]);
+           // colorChoice.style("backgroundColor",colorArray[c] )
+          }
+
+          colorChoice.position(width / 2, height / 2 + 150);
+          colorChoice.style("height", "250");
+          colorChoice.hide();
+        }
+      });
+
       form = new Form();
       form.display();
     }
   }
   play() {
     form.hide();
-    background(rgb(198, 135, 103));
     textAlign(CENTER);
+if(player.color!==""){
+  push()
+  rectMode(CENTER)
+  fill(player.color)
+  rect(width/2,60,width,120)
+pop()
+}
     var playerScores = [];
     if (allPlayers != undefined) {
       for (var plr in allPlayers) {
@@ -169,7 +198,11 @@ class Game {
     this.displayScores(displayWidth - 500, 100, 15, this.playerScores);
     textSize(30);
     text("Round " + player.round, width / 2, height / 2 - 300);
-    if (gameMode == "i") {
+    if (gameMode == "cc") {
+      this.chooseColor();
+    } else if (gameMode == "c") {
+      this.waitForColor();
+    } else if (gameMode == "i") {
       this.initialize();
     } else if (gameMode == "q") {
       this.displayQuestion();
@@ -186,7 +219,64 @@ class Game {
       this.roundResults();
     }
   }
+  chooseColor() {
+    if (player.color == "") {
+      colorChoice.show();
+    }
+    player.color = colorChoice.value();
+    if (colorArray != undefined) {
+      //  console.log(colorArray);
 
+      var cArray = [...colorArray];
+      for (var i = cArray.length - 1; i >= 0; i--) {
+        //console.log(cArray[i]);
+
+        if (cArray[i] === player.color) {
+          console.log(cArray[i]);
+
+          cArray.splice(i, 1);
+          console.log(cArray);
+        }
+      }
+      database.ref("/").update({
+        colorArray: cArray,
+      });
+      player.update();
+    }
+    if (player.color != "") {
+      colorChoice.hide();
+      player.round = 1;
+      player.update();
+      gameMode = "c";
+    }
+  }
+  waitForColor() {
+    clear();
+    // colorChoice.show();
+    //console.log("color screen");
+    if (rounds === true) {
+      colorChoice.hide();
+      gameMode = "i";
+    }
+
+    rounds = true;
+
+    var index = 0;
+
+    for (var plr in allPlayers) {
+      index++;
+      if (allPlayers[plr].active && allPlayers[plr].round != 1) {
+        text(
+          "Waiting for " +
+            allPlayers[plr].name +
+            " to finish selecting their color ",
+          width / 2,
+          index * 50 + 150
+        );
+        rounds = false;
+      }
+    }
+  }
   initialize() {
     this.questionEle.hide();
     clear();
@@ -361,7 +451,7 @@ class Game {
         }
       }
 
-      text("Round " + player.round+" "+ this.roundType, width / 2, height / 2 - 50);
+      text("Round " + player.round, width / 2, height / 2 - 50);
 
       gameMode = "roundresult";
     } else {
@@ -369,8 +459,7 @@ class Game {
     }
   }
   roundResults() {
-    text("Round " + (player.round - 1)
-     + " Results", width / 2, height / 2 - 50);
+    text("Round " + player.round - 1 + " Results", width / 2, height / 2 - 50);
 
     this.displayScores(width / 2, height / 2, 30, this.playerScores);
 
@@ -381,35 +470,35 @@ class Game {
   gameRound1() {
     this.maxPlayers = 15;
     this.minPlayers = 2;
-    this.totalQuestions = 10;
-    this.questionWait = 10;
-    this.answerWait = 5;
-    this.questionNumber = 0;
-    this.roundType = "normal";
-
-    this.difficulty = "easy";
-  }
-  gameRound2() {
-    this.maxPlayers = 10;
-    this.minPlayers = 4;
-    this.totalQuestions = 10;
-    this.questionWait = 10;
-    this.answerWait = 5;
-    this.questionNumber = 0;
-    this.roundType = "normal";
-
-    this.difficulty = "easy";
-  }
-  gameRound3() {
-    this.maxPlayers = 4;
-    this.minPlayers = 2;
-    this.totalQuestions = 5;
+    this.totalQuestions = 3;
     this.questionWait = 5;
     this.answerWait = 5;
     this.questionNumber = 0;
     this.roundType = "fast";
 
-    this.difficulty = "medium";
+    this.difficulty = "easy";
+  }
+  gameRound2() {
+    this.maxPlayers = 4;
+    this.minPlayers = 4;
+    this.totalQuestions = 3;
+    this.questionWait = 5;
+    this.answerWait = 5;
+    this.questionNumber = 0;
+    this.roundType = "fast";
+
+    this.difficulty = "easy";
+  }
+  gameRound3() {
+    this.maxPlayers = 2;
+    this.minPlayers = 2;
+    this.totalQuestions = 3;
+    this.questionWait = 5;
+    this.answerWait = 5;
+    this.questionNumber = 0;
+    this.roundType = "fast";
+
+    this.difficulty = "easy";
   }
   gameRound4() {
     this.maxPlayers = 2;
@@ -419,7 +508,7 @@ class Game {
     this.answerWait = 5;
     this.questionNumber = 0;
     this.roundType = "fast";
-    this.difficulty = "medium";
+    this.difficulty = "easy";
   }
   displayScores(x, y, size, arr) {
     Player.getPlayerInfo();
@@ -433,9 +522,9 @@ class Game {
         if (!arr[i][3]) {
           fill("grey");
         } else if (arr[i][2] === player.index) {
-          fill("green");
+          fill(player.color);
         } else {
-          fill("black");
+          fill("brown");
         }
 
         text(arr[i][0] + ": " + arr[i][1], x, y);
@@ -464,7 +553,6 @@ class Game {
         ]);
       }
     }
-    console.log(this.playerScores);
     this.playerScores = playerScores;
     this.playerScores.sort((a, b) => {
       return b[1] - a[1];
